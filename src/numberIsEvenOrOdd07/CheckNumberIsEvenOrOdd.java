bdfa85afed5ea14f6803d68b62fda74cd6cd334c
miladkth <116656210+miladkth@users.noreply.github.com>
2025-11-13 16:28:40 +0100

    checking number is even or odd using ternary operator - ex nr 07

---
+++ src/numberIsEvenOrOdd07/CheckNumberIsEvenOrOdd.java
@@ -0,0 +1,14 @@
+package numberIsEvenOrOdd07;
+
+import java.util.Scanner;
+
+public class CheckNumberIsEvenOrOdd {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+        String evenOdd = (num%2==0)? "Even" : "Odd";
+
+        System.out.println(num + " is " + evenOdd);
+    }
+}
