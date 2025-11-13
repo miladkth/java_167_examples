cc74e244b39d1a6935c2f655588e4c1f2f90def5
miladkth <116656210+miladkth@users.noreply.github.com>
2025-11-13 16:18:11 +0100

    swap to numbers using temporary - ex nr 06

---
+++ src/swapToNumbers06/SwapToNumbers.java
@@ -0,0 +1,25 @@
+package swapToNumbers06;
+
+public class SwapToNumbers {
+    public static void main(String[] args) {
+        float first = 1.25f, second = 2.45f;
+        System.out.println("--Before swap--");
+        System.out.println("First number: " + first);
+        System.out.println("Second number: " + second);
+
+        //Value of first assigned to temporary
+        float temporary = first;
+
+        //Value of second assigned to first
+        first = second;
+
+        //Value of temporary (which contains the initial value of first) is assigned to second
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number: " + first);
+        System.out.println("Second number: " + second);
+
+
+    }
+}
