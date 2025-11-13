406131ebd53da1741dada355d471af792e86f7c7
miladkth <116656210+miladkth@users.noreply.github.com>
2025-11-13 14:14:29 +0100

    add two floating point number - ex nr 03

---
+++ src/addTwoFloatingPointNumber03/AddTwoFloatingPointNumber.java
@@ -0,0 +1,12 @@
+package addTwoFloatingPointNumber03;
+
+public class AddTwoFloatingPointNumber {
+    public static void main(String[] args) {
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
