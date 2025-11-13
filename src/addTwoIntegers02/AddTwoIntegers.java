5ec7dd8a3e7d04ba6286eb6a7f92a3f54a7e2f93
miladkth <116656210+miladkth@users.noreply.github.com>
2025-11-13 14:08:53 +0100

    add two integers ex nr 2

---
+++ src/addTwoIntegers02/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+package addTwoIntegers02;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        int first = 5;
+        int second = 3;
+
+        int sum = first + second;
+
+        System.out.println(first + " + " + second + " = " + sum);
+    }
+}
