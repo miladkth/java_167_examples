d3ce025e9a2113feb0ba1b91295d12c3b3386c61
miladkth <116656210+miladkth@users.noreply.github.com>
2025-11-21 14:10:25 +0100

    added an Character() method - still ex nr 13

--- src/checkCharacterIfAlphabetOrNot13/CharAlphabetOrNot.java
+++ src/checkCharacterIfAlphabetOrNot13/CharAlphabetOrNot.java
@@ -2,10 +2,17 @@ package checkCharacterIfAlphabetOrNot13;
 
 public class CharAlphabetOrNot {
     public static void main(String[] args) {
-        char c = '*';
+        char c = 'a';
 
         String result = ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? "is an alphabet" : "is not an alphabet";
         System.out.println(c + " " + result);
+
+
+        if (Character.isAlphabetic(c)) {
+            System.out.println(c + " is an alphabet");
+        } else {
+            System.out.println(c + " is not an alphabet");
+        }
     }
 }
 
