78a0aca2cfa8ae4b1dff664a626200968e336e73
miladkth <116656210+miladkth@users.noreply.github.com>
2025-11-24 21:02:45 +0100

    Displaied alphabet using for loop - ex nr 20

---
+++ src/displayAlphabets20/DisplayAlphabet.java
@@ -0,0 +1,11 @@
+package displayAlphabets20;
+
+public class DisplayAlphabet {
+    public static void main(String[] args) {
+        char c = '*';
+
+        for (c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c + " ");
+        }
+    }
+}
