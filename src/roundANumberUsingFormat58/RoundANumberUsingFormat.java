0c7ad83419816e35963096188a9627b1df757bb9
miladkth <116656210+miladkth@users.noreply.github.com>
2026-01-12 14:29:03 +0100

    program to round a number to n decimal using format - ex nr 58

---
+++ src/roundANumberUsingFormat58/RoundANumberUsingFormat.java
@@ -0,0 +1,9 @@
+package roundANumberUsingFormat58;
+
+public class RoundANumberUsingFormat {
+    public static void main(String[] args) {
+        double num = 1.34567;
+
+        System.out.format("%.4f", num);
+    }
+}
