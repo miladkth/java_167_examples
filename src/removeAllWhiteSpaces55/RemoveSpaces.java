5323b9b635fef8aa9a1978b8e1a630dda4c91995
miladkth <116656210+miladkth@users.noreply.github.com>
2026-01-12 14:02:41 +0100

    program to remove all whitespaces from a string

---
+++ src/removeAllWhiteSpaces55/RemoveSpaces.java
@@ -0,0 +1,12 @@
+package removeAllWhiteSpaces55;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String sentence = "T        his  is b     ett                  er";
+        System.out.println("Original sentence: " + sentence);
+        sentence = sentence.replaceAll("\\s","");
+        //sentence = sentence.replace(" ", "");
+        //sentence = sentence.replace("|", " ");
+        System.out.println("After replacement: " + sentence);
+    }
+}
